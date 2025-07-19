3d7b18390fdf938945d53a5cba67ec85699bb33a
Brian Baggs <brianbaggs@hotmail.com>
2025-07-19 09:55:51 -0300

    Modify home page

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Food Recipes</h1>
-        <p className="lead">
-          A curated list of recipes for the best homemade meal and delicacies.
+  <div className="w-screen h-screen bg-primary flex items-center justify-center">
+    <div className="bg-transparent">
+      <div className="container text-secondary">
+        <h1 className="text-5xl font-bold mb-4">Test App</h1>
+        <p className="text-xl mb-4">
+          Test application for using React on Rails.
         </p>
-        <hr className="my-4" />
+        <hr className="my-4 border-t border-gray-300" />
         <Link
-          to="/recipes"
-          className="btn btn-lg custom-button"
+          to="/newlink"
+          className="btn btn-lg bg-blue-600 text-white px-6 py-3 rounded shadow hover:bg-blue-700 transition"
           role="button"
         >
-          View Recipes
+          Link Test
         </Link>
       </div>
     </div>
